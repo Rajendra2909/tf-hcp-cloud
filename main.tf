784201c5aff4d69677663db9586b923c9cdc538c
Rajendra More <RajendraM@jmf.com>
2026-03-01 16:48:55 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform { 
+  cloud { 
+    
+    organization = "reyuorg" 
+
+    workspaces { 
+      name = "tf-hcp-cloud" 
+    } 
+  } 
+}
+provider "aws" {
+  region = var.region
+}
+resource "aws_vpc" "vcs_driven_vpc" {
+  cidr_block = var.cidr_block
+  enable_dns_hostnames = "true"
+  enable_dns_support = "true"
+  tags = {
+    Name = "vcs_driven_vpc"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "vcs_driven_vpc" {
+  value = aws_vpc.vcs_driven_vpc.id
+}
